0e0ea60c6ebb2fab1aa8cb744db3c523f289c073
Arielle <seohee70428@gmail.com>
2023-02-26 20:46:35 +0900

    feat: update all code in calendar

---
+++ app/src/main/java/com/example/calendar_new/FindfriendsActivity.kt
@@ -0,0 +1,13 @@
+package com.example.calendar_new
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class FindfriendsActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.find_friends)
+    }
+
+}
+
---
+++ app/src/main/java/com/example/calendar_new/PrivateMonthRatioInterface.kt
@@ -0,0 +1,15 @@
+package com.example.calendar_new
+
+import com.example.calendar_new.model.MyData
+import retrofit2.Call
+import retrofit2.http.*
+
+interface PrivateMonthRatioInterface {
+    @Headers("accept: application/json", "content-type: application/json")
+    @GET("/api/v1/private/todo/monthly/ratio/{date}") //보내는 정보
+    fun getData(
+        @Header("Authorization") token:String,
+        @Path("date") date:String
+    ): Call<MyData>
+}
+
