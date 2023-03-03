7394f847ba8d542078cc4784f5e3599b16cabe2d
Arielle <seohee70428@gmail.com>
2023-03-04 03:04:14 +0900

    Feat. 캘린더 하단 이미지 수정 및 옵션메뉴 추가

---
+++ app/src/main/java/com/example/greendar/ui/view/ShowAccountAddfriendsActivity.kt
@@ -0,0 +1,17 @@
+package com.example.greendar.ui.view
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.greendar.R
+
+class ShowAccountAddfriendsActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.show_account_addfriends)
+    }
+}
+
+/*친구추가하기 API 연결 되면 추가 된 친구가 바로 show_account_addfriends 레이아웃에 추가됨*/
+
+
+
