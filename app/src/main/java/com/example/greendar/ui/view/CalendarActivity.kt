d15813ecfe3182da63869f080975080b5757f427
Arielle <seohee70428@gmail.com>
2023-03-10 18:38:18 +0900

    Feat: 프로필세팅 intent값 받아오기

--- app/src/main/java/com/example/greendar/ui/view/CalendarActivity.kt
+++ app/src/main/java/com/example/greendar/ui/view/CalendarActivity.kt
@@ -1,10 +1,14 @@
 package com.example.greendar.ui.view
 
 import android.annotation.SuppressLint
-import android.content.Context
 import android.content.Intent
+import android.graphics.Canvas
+import android.graphics.Color
+import android.graphics.Paint
+import android.graphics.drawable.Drawable
 import android.net.Uri
 import android.os.Bundle
+import android.text.style.LineBackgroundSpan
 import android.util.Log
 import android.view.Menu
 import android.view.MenuInflater
@@ -15,15 +19,16 @@ import android.widget.TextView
 import android.widget.Toast
 import com.example.greendar.R
 import com.example.greendar.data.api.PrivateMonthRatioInterface
-import com.example.greendar.data.model.MyData
-import com.prolificinteractive.materialcalendarview.CalendarDay
 import com.prolificinteractive.materialcalendarview.MaterialCalendarView
-import com.prolificinteractive.materialcalendarview.OnDateSelectedListener
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.content.ContextCompat
-//import com.example.calendar_new.FindfriendsActivity
-//import com.example.calendar_new.SettingsActivity
-import com.prolificinteractive.materialcalendarview.*
+import com.example.greendar.data.api.EventTodoRatioInterface
+import com.example.greendar.model.EventTodoRatio
+import com.example.greendar.model.MyData
+import com.prolificinteractive.materialcalendarview.CalendarDay
+import com.prolificinteractive.materialcalendarview.DayViewDecorator
+import com.prolificinteractive.materialcalendarview.DayViewFacade
+import com.prolificinteractive.materialcalendarview.spans.DotSpan
 import retrofit2.Call
 import retrofit2.Callback
 import retrofit2.Response
@@ -39,9 +44,9 @@ class CalendarActivity : AppCompatActivity() {
 
     //[사진,텍스트 새로고침]
     lateinit var textView: TextView
-    lateinit var imageView:ImageView
+    private lateinit var imageView:ImageView
     lateinit var imageButton: ImageButton
-    val changeImages: IntArray = intArrayOf(
+    private val changeImages: IntArray = intArrayOf(
         R.drawable.cheetah,
         R.drawable.giantotter,
         R.drawable.polarbear,
@@ -49,15 +54,15 @@ class CalendarActivity : AppCompatActivity() {
         R.drawable.vendacycad,
         R.drawable.holly
     )
-    val ImageTitle = mutableListOf("Cheetah","GiantOtter","PolarBear","Staghorn","Venda Cycad","Holly")
-    val ImageG2 = mutableListOf(
+    private val imageTitle = mutableListOf("Cheetah","GiantOtter","PolarBear","Staghorn","Venda Cycad","Holly")
+    private val imageG2 = mutableListOf(
         "Algeria, Angola(Savana)",
         "Amazon(Forest, Marine Neritic)",
         "Canada, Greenland, Norway, Alaska",
         "Anguilla(Marine Neritic)",
         "SouthAfrica(Savana)",
         "Albania(Forest, Shrub land)"
-        )
+    )
 
 
     @SuppressLint("MissingInflatedId")
@@ -68,116 +73,86 @@ class CalendarActivity : AppCompatActivity() {
         //[사진,텍스트 새로고침]
         val titletextView = findViewById<TextView>(R.id.holly_title)
         val rangetextView = findViewById<TextView>(R.id.holly_geographic_range_2)
-        imageView = findViewById(R.id.holly_fix);
+        imageView = findViewById(R.id.holly_fix)
         imageButton = findViewById(R.id.image_change_btn)
 
         var index = 0
 
         imageButton.setOnClickListener{
             imageView.setImageResource(changeImages[index])
-            titletextView.text = ImageTitle[index]
-            rangetextView.text = ImageG2[index]
+            titletextView.text = imageTitle[index]
+            rangetextView.text = imageG2[index]
             index = (index +1) % changeImages.size
         }
 
 
+        //[캘린더] 1. 변수선언 및 특정 날짜(2023-3-10)에 도트 찍기
+        val calendarView: MaterialCalendarView = findViewById(R.id.calendarView)
+        Log.d("hhh","dot check1")
+        val date : CalendarDay = CalendarDay.from(2023,3,13)
+        Log.d("hhh","dot check2")
+        val dot : Drawable? = ContextCompat.getDrawable(this,R.drawable.dot)
+
+        class DotDecorator(private val drawable: Drawable, private val date: CalendarDay) : DayViewDecorator{
+            override fun shouldDecorate(day: CalendarDay): Boolean {
+                return day == date // date에 지정한 날짜에만 도트 찍기 True로 반환
+            }
+            override fun decorate(view: DayViewFacade) {
+                val dotSize = 10 // 점의 크기
+                val dotColor = Color.BLUE// 점의 색상
+                val dotSpan = DotSpan(dotSize.toFloat(), dotColor)
+                view.addSpan(dotSpan)
+                Log.d("hhh","dot check3")
+            }
+        }
+        val decorator = DotDecorator(dot!!, date)
+        calendarView.addDecorator(decorator)
+        Log.d("hhh","dot check4")
+
         //[캘린더] 1. 변수선언
-        val calendarView: MaterialCalendarView = findViewById(R.id.calendarview)
         val selectedDateTextView: TextView = findViewById(R.id.day_text)
         val selectedMonthTextView: TextView = findViewById(R.id.month_text)
+        val selectedEventMonthTextView : TextView = findViewById(R.id.eventTODO_month_text)
 
         //[API] token, date 변수 선언
-        var month:String ="1"
-        var date: String = "1"
+        var month: String
 
         //외부 사이크 링크 연결 변수선언
-        val redlist_click_btn: ImageButton = findViewById(R.id.redlist_click_btn)
-
-
-
-        /*Run Affect X
-        //initialize the selected month text view to display the current month
-        */
-        /**
-         * SimpleDateFormat : Date -> String
-         *//*
-        val currentDate = CalendarDay.today()
-        val currentMonth = SimpleDateFormat("yyyy-MM-dd", Locale.getDefault()).format(currentDate.date)
-        selectedMonthTextView.text = currentMonth*/
-
+        val redlistclickbtn: ImageButton = findViewById(R.id.redlist_click_btn)
 
 
         //[캘린더] 2. 텍스트뷰에 불러오기(Date/일자별) : set up the DATE changed listener to update the selected month text view
-        //[Bar 아이콘 넣기] 1. DayViewDecorator 인터페이스 사용해서 선택된 날짜에 아이콘 추가하기
-        //[그냥 아이콘만 찍힘]
-            class SpecialDayDecorator(context: Context) : DayViewDecorator {
-            private val drawable = ContextCompat.getDrawable(context, R.drawable.ic_leaf)!!
-            private var specialDate: CalendarDay? = null
+        calendarView.setOnDateChangedListener { _, date, _ ->
+            val formattedDate =
+                SimpleDateFormat("yyyy-MM-dd", Locale.getDefault()).format(date.date)
+            selectedDateTextView.text = formattedDate
+            formattedDate.toString()
 
-            fun setSpecialDate(date:CalendarDay){
-                specialDate = date
-            }
-            //[Bar 아이콘 넣기] shouldDecorate() : 날짜에 아이콘 넣기 ★조건★ 결정 : True/False 반환
-            override fun shouldDecorate(day: CalendarDay?): Boolean {
-                return day == specialDate
-                return day == specialDate
-            }
-            override fun decorate(view: DayViewFacade?) {
-                view?.setSelectionDrawable(drawable)
-            }
+            //날짜 클릭 시 해당 TodoActivity 페이지로 전환 , formattedDate 데이터 전달
+            val intent = Intent(this@CalendarActivity, TodoActivity::class.java)
+            intent.putExtra("selectedDate", formattedDate)
+            Log.d("hhh", formattedDate.toString()) // 클릭 날짜 잘 넘어감 확인
+            startActivity(intent)
         }
-        val selectedDateDecorator = SpecialDayDecorator(this)
-        calendarView.addDecorator(selectedDateDecorator)
 
 
-        /*//[Bar 아이콘 넣기] 2. OnDateChangedListner 이용해서 선택된 날짜에 아이콘 넣기
-        calendarView.setOnDateChangedListener(object : OnDateSelectedListener {
-            override fun onDateSelected(
-                widget: MaterialCalendarView,
-                date: CalendarDay,
-                selected: Boolean
-            ) {
-                val formattedDate =
-                    SimpleDateFormat("yyyy-MM-dd", Locale.getDefault()).format(date?.date)
-                selectedDateTextView.text = formattedDate
-
-                selectedDateDecorator.setSpecialDate(date)
-                // 선택된 날짜의 셀에 Span을 추가
-                calendarView.invalidateDecorators()
-            }
-        })*/
-
-
-        //[원래 했던 것 : 그냥 날짜 클릭 시 하단 텍스트 뷰에 해당 날짜 띄우는 형태]
-        calendarView.setOnDateChangedListener(object : OnDateSelectedListener {
-            override fun onDateSelected(
-                widget: MaterialCalendarView,
-                date: CalendarDay,
-                selected: Boolean
-            ) {
-                val formattedDate =
-                    SimpleDateFormat("yyyy-MM-dd", Locale.getDefault()).format(date?.date)
-                selectedDateTextView.text = formattedDate
-
-            }
-        })
-
-
-        //[캘린더] 2. 텍스트뷰에 불러오기(Month): set up the MONTH changed listener to update the selected month text view
-        calendarView.setOnMonthChangedListener { widget, date ->
+        //[캘린더] 2. 텍스트뷰에 불러오기(Month): Event, Private 둘다
+        calendarView.setOnMonthChangedListener { _, date ->
             val formattedMonth =
-                SimpleDateFormat("yyyy-MM-dd", Locale.getDefault()).format(date?.date)
+                SimpleDateFormat("yyyy-MM-dd", Locale.getDefault()).format(date?.date!!)
             selectedMonthTextView.text = formattedMonth
+            selectedEventMonthTextView.text=formattedMonth
             month=formattedMonth.toString()
-            getMyData(month)
+            getMyData(month) //API
+            getEventTodoRatio(month)//API
 
         }
+
         //[링크] 캘린더 하단 ICUN 링크 연결
-        redlist_click_btn.setOnClickListener {
+        redlistclickbtn.setOnClickListener {
             val openURL = Intent(Intent.ACTION_VIEW)
             openURL.data = Uri.parse("https://www.iucnredlist.org/")
             startActivity(openURL)
-
         }
     }
 
@@ -218,16 +193,18 @@ class CalendarActivity : AppCompatActivity() {
             .baseUrl(BASE_URL)
             .build()
             .create(PrivateMonthRatioInterface::class.java)
-        Log.d("hhh", "연결 확인 중2")
+        Log.d("hhh", "Private 연결 확인 중1")
 
 
-        /*토큰으로 사용자 구별해서 완료 비율을 띄우는겨*/
-        val retrofitData = retrofitBuilder.getData(token = "222222" , date = month)
+        //ProfileSetting 183 Token Receive
+        val token : String? = intent.getStringExtra("token")
+        val retrofitData = retrofitBuilder.getData(token = token.toString(), date = month)
+        Log.d("hhh", "Private Token : $token")
 
 
         retrofitData.enqueue(object : Callback<MyData?> {
             override fun onResponse(call: Call<MyData?>, response: Response<MyData?>) {
-                Log.d("hhh", "연결 확인 중3")
+                Log.d("hhh", "Private 연결 확인 중2")
                 Log.d("hhh", "${response.code()}")
 
 
@@ -238,16 +215,10 @@ class CalendarActivity : AppCompatActivity() {
                     val ratioList = bodyList?.map { it.ratio }
                     val ratioSum = ratioList?.sum()
 
-                   /* //ratioSum을 Bar 형태로 나타내기
-                    val progressBar : ProgressBar = findViewById(R.id.progress_bar)
-                    progressBar.progress = (ratioSum?: 0) as Int
-                    progressBar.max = 100*/
-
-                    /*val result : String = "PRIVATE TO-DO COMPLETION RATE is ${ratioSum ?: 0}%"*/
-                    val result : String = "PRIVATE TO-DO COMPLETION RATE is ${ratioSum}%"
+                    val result = "PRIVATE TO-DO COMPLETION RATE is ${ratioSum}%"
                     val selectedMonthTextView: TextView = findViewById(R.id.month_text)
-                    selectedMonthTextView.text = result.toString()
-                    Log.d("hhh", "연결 확인 중4")
+                    selectedMonthTextView.text = result
+                    Log.d("hhh", "Private 연결 확인 중3")
                 }else{
                     Log.d("hhh", "연결 실패")
                 }
@@ -263,8 +234,53 @@ class CalendarActivity : AppCompatActivity() {
 
     }
 
-}
+    //[API] @Get EventTodoRatioInterface 연결
+    private fun getEventTodoRatio(month:String){
+        val retrofitBuilder = Retrofit.Builder()
+            .addConverterFactory(GsonConverterFactory.create())
+            .baseUrl(BASE_URL)
+            .build()
+            .create(EventTodoRatioInterface::class.java)
+        Log.d("hhh", "Event 연결 확인 중1")
 
+
+        //ProfileSetting 183 Token Receive
+        val token : String? = intent.getStringExtra("token")
+        val retrofitData = retrofitBuilder.getData(token = token.toString() , date = month)
+        Log.d("hhh", "Event Token : $token")
+
+
+        retrofitData.enqueue(object : Callback<EventTodoRatio?> {
+            override fun onResponse(call: Call<EventTodoRatio?>, response: Response<EventTodoRatio?>) {
+                Log.d("hhh", "Event 연결 확인 중2")
+                Log.d("hhh", "${response.code()}")
+
+
+                if(response.isSuccessful){
+                    //ratio만 리스트(bodyList 변수로 선언)로 받아오려고 추가한 변수들
+                    val myData = response.body()
+                    val bodyList = myData?.body
+                    val ratioList = bodyList?.map { it.ratio }
+                    val ratioSum = ratioList?.sum()
+
+                    val result = "EVENT TO-DO COMPLETION RATE is ${ratioSum}%"
+                    val selectedEventMonthTextView: TextView = findViewById(R.id.month_text)
+                    selectedEventMonthTextView.text = result
+                    Log.d("hhh", "Event 연결 확인 중3")
+                }else{
+                    Log.d("hhh", "연결 실패")
+                }
+            }
+            override fun onFailure(call: Call<EventTodoRatio?>, t: Throwable) {
+                Toast.makeText(this@CalendarActivity, "서버 로직 에러", Toast.LENGTH_LONG).show()
+                Log.d("hhh", "서버 로직 에러")
+            }
+        })
+
+
+    }
+
+}
 
 
 
